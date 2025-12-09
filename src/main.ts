70fca5e97e19efa8c7240a18be15687142f2f043
Sergey Pomortsev <zaooooza@gmail.com>
2025-12-10 00:15:26 +0300

    fix import bug

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import "./style.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import 'ag-grid-enterprise';
 
 createApp(App).mount("#app");
 
